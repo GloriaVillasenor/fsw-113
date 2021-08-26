711b9c1398adf4718fba0627d801311925de0086
GLORIA VILLASENOR <82358971+GloriaVillasenor@users.noreply.github.com>
2021-08-25 18:47:37 -0700

    Added languages2 to week4

--- week4/assignments/langs/languages2.js
+++ week4/assignments/langs/languages2.js
@@ -1,17 +1,27 @@
 const lang = 'JavaScript'
 
-// Create an event listener for the submit button that adds all 'input' elements to 
-// a single array using the spread operator. Call the chkLang() function, passing in 
-// the array as an argument.
 document.querySelector("#submit").addEventListener("click", function(){
-    chkLang()
+    const myInputs = document.querySelectorAll('input')
+
+    console.log('myInputs', myInputs)
+
+    const allarry = [...myInputs]
+
+    console.log('allarry', allarry)
+
+    chkLang(allarry)
 })
 
 function chkLang(langs) {
     let result = false
 
-    // use an array method to check whether the user included 'JavaScript' in their
-    // list of languages
+    langs.forEach((curLang) => {
+
+        if(curLang.value === lang) {
+            result = true
+        }
+
+    })
 
     let obj = document.querySelector('#TestResult')
     if (result) 
